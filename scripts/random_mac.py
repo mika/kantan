84f1c4c1a5ac47dad0624a6c79c4dd887a5cb0ca
Michael Prokop <mika@grml.org>
2010-12-13 17:16:11 +0100

    Do not completely randomize MAC address, use 00:0c:29 for first three bytes to be ignored by udev's 75-persistent-net-generator.rules.
    To avoid generation of persistent NIC device names in udev's configuration
    accross reboots let's assign a MAC address that's known to be ignored by
    /lib/udev/rules.d/75-persistent-net-generator.rules - therefore
    always start mac address with 00:0c:29 and randomize just the last three
    bytes of the 6 bytes/48 bits.

--- scripts/random_mac.py
+++ scripts/random_mac.py
@@ -1,7 +1,12 @@
 #! /usr/bin/python
 import random
 
-mac = [ 0x00, 0x16, 0x3e,
+# To avoid generation of persistent NIC device names in udev's configuration
+# accross reboots let's assign a MAC address out of VMware's range as it's
+# known to be ignored by /lib/udev/rules.d/75-persistent-net-generator.rules ->
+# ENV{MATCHADDR}=="00:0c:29:*|00:50:56:*", ENV{MATCHADDR}=""
+
+mac = [ 0x00, 0x0c, 0x29,
   random.randint(0x00, 0x7f),
   random.randint(0x00, 0xff),
   random.randint(0x00, 0xff) ]
